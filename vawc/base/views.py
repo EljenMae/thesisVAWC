b0436a27f4f90f6da43ff3f174422e4e6d014e00
Eljen Mae Augusto <xt202001173@wmsu.edu.ph>
2023-11-06 21:44:18 +0800

    smoll changes

--- vawc/base/views.py
+++ vawc/base/views.py
@@ -6,7 +6,7 @@ def home(request):
     return render(request, 'vawc/home.html')
 
 def report(request):
-    return render(request, 'report.html')
+    return render(request, 'vawc/report.html')
 
 def dswd(request):
     return render(request, 'dswd-dashboard.html')
