9c6204455f3291b18e61320e1a0f63f40ebf6998
Eljen Mae Augusto <xt202001173@wmsu.edu.ph>
2023-11-06 13:24:26 +0800

    some trash to add

--- vawc/base/views.py
+++ vawc/base/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'vawc/home.html')
 
 def report(request):
     return render(request, 'report.html')
 
 def dswd(request):
     return render(request, 'dswd-dashboard.html')
+
