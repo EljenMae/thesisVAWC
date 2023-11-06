53bde76a537630eb34fd6c36b8557e1a83cd25e8
Eljen Mae Augusto <xt202001173@wmsu.edu.ph>
2023-11-06 20:51:24 +0800

    some adding things

--- vawc/base/admin.py
+++ vawc/base/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Barangay
+from .models import Agency
 
-admin.site.register(Barangay)
+admin.site.register(Agency)
+
--- vawc/base/models.py
+++ vawc/base/models.py
@@ -2,17 +2,24 @@ from django.db import models
 
 # Create your models here.
 
-class Barangay(models.Model):
-    barangayName = models.CharField(max_length=200)
-    barangayAddress = models.CharField(max_length=200, blank=True)
-    fullname = models.CharField(max_length=200)
-    #barangay = models.CharField(max_length=200)
-    contactNum = models.CharField(max_length=20)
+
+  
+class Agency(models.Model):
+    agencyName = models.CharField(max_length=200)
+    agencyAddress = models.CharField(max_length=200, blank=True)
+    contactPerson = models.CharField(max_length=200)
     email = models. CharField(max_length=100, blank=True)
+    contactNum = models.CharField(max_length=20)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.fullname
     
+class Barangay(models.Model):
+    #user = 
+    agency = models.ForeignKey(Agency, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
     
