1f82fac46f3841002edb7f32135208742e26f1a9
Eljen Mae Augusto <xt202001173@wmsu.edu.ph>
2023-11-09 09:09:11 +0800

    sign up done

--- vawc/base/urls.py
+++ vawc/base/urls.py
@@ -5,5 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('report/', views.report, name="report"),
+    path('login/', views.login, name="login"),
+    path('signup/', views.signup, name="signup"),
+    path('barangay/dashboard', views.barangay, name="barangay"),
+    path('police/dashboard', views.police, name="police"),
     path('dswd/dashboard', views.dswd, name="dswd"),
+    
 ]
--- vawc/base/views.py
+++ vawc/base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 
+
 # Create your views here.
 def home(request):
     return render(request, 'vawc/home.html')
@@ -8,6 +9,17 @@ def home(request):
 def report(request):
     return render(request, 'vawc/report.html')
 
+def login(request):
+    return render(request, 'vawc/login.html')
+
+def signup(request):
+    return render(request, 'vawc/signup.html')
+
+def barangay(request):
+    return render(request, 'vawc/barangay-dashboard.html')
+
+def police(request):
+    return render(request, 'vawc/police-dashboard.html')
+
 def dswd(request):
     return render(request, 'vawc/dswd-dashboard.html')
-
